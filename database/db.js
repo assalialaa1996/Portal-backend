e05b21e6970516c3527539a8fcf9f28a807cd2d9
Alaa <alaa.assali1996@outlook.com>
2020-03-11 21:10:09 +0100

    commit

---
+++ database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb://azerty:azerty12345678@cluster0-shard-00-00-edmjh.mongodb.net:27017,cluster0-shard-00-01-edmjh.mongodb.net:27017,cluster0-shard-00-02-edmjh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
