5636d7a13e585d4b5bb229449535fe9e481a34e8
Alaa <alaa.assali1996@outlook.com>
2020-03-26 14:10:22 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://azerty:azerty12345678@cluster0-shard-00-00-edmjh.mongodb.net:27017,cluster0-shard-00-01-edmjh.mongodb.net:27017,cluster0-shard-00-02-edmjh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    db: 'mongodb://The50cent:The50cent@cluster0-shard-00-00-ynd2e.mongodb.net:27017,cluster0-shard-00-01-ynd2e.mongodb.net:27017,cluster0-shard-00-02-ynd2e.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
